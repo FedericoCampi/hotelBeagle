f710ee784e36e9c8428ed5e72c3b8ec406cc627b
Federico Campi <federicocampi95@gmail.com>
2023-08-27 21:07:14 +0100

    arrows

--- src/components/HabMobile.jsx
+++ src/components/HabMobile.jsx
@@ -1,7 +1,11 @@
 import hab3adult from '../images/hab3adult.jpg'
 import suite from '../images/suite.jpg'
 
-const habitaciones = [
+import { MdArrowBackIos, MdArrowForwardIos } from 'react-icons/md'
+import '../assets/style.css';
+import { useEffect, useRef, useState } from 'react';
+
+  const habitaciones = [
     {
       "id": 1,
       "img": "https://static.wixstatic.com/media/ec3017_964571869076490dae215f6046eee4ad~mv2.jpg/v1/fill/w_926,h_444,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/ec3017_964571869076490dae215f6046eee4ad~mv2.jpg",
@@ -27,13 +31,71 @@ const habitaciones = [
 
 const HabMobile = () => {
 
+  const firstDivRef = useRef(null);
+  const thirdDivRef = useRef(null);
+  const observer = useRef(null);
+
+  const [arrowBack, setArrowBack] = useState(true);
+  const [arrowFoward, setArrowFoward] = useState(true);
+
+  useEffect(() => {
+    observer.current = new IntersectionObserver(entries => {
+      entries.forEach(entry => {
+        if (entry.target === firstDivRef.current) {
+          if (entry.isIntersecting) {
+            setArrowBack(false)
+          } else {
+            setArrowBack(true)
+          }
+        }
+        if (entry.target === thirdDivRef.current) {
+          if (entry.isIntersecting) {
+            setArrowFoward(false)
+          } else {
+            setArrowFoward(true)
+          }
+        }
+      });
+    });
+
+    if (firstDivRef.current) {
+      observer.current.observe(firstDivRef.current);
+    }
+    if (thirdDivRef.current) {
+      observer.current.observe(thirdDivRef.current);
+    }
+
+    return () => {
+      if (observer.current) {
+        observer.current.disconnect();
+      }
+    };
+    
+  }, []);
+
     return (
         <div className="overflow-x-scroll snap-x snap-mandatory flex">
-            {habitaciones?.map(item => (
-            <div key={item.id} className='flex-shrink-0 w-screen p-8 snap-center'>
+            {habitaciones?.map((item, index)=> (
+              <div key={item.id} className='flex-shrink-0 w-screen p-8 snap-center'
+              >
+                <div
+                  className={`${arrowBack === false ? 'hidden' : ''} absolute top-[50%] left-3 animate-hbounce`}
+                  // className="" 
+                  style={{ animationDuration: '2s', animationIterationCount: 'infinite' }}
+                  >
+                  <MdArrowBackIos className='text-green-800 text-5xl'/>
+                </div>
+                <div
+                  className={`${arrowFoward === false ? 'hidden' : ''} absolute top-[50%] right-3 animate-hbounce`}
+                  // className="" 
+                  style={{ animationDuration: '2s', animationIterationCount: 'infinite' }}
+                  >
+                  <MdArrowForwardIos className='text-green-800 text-5xl'/>
+                </div>
                 <img src={item.img} alt={item.tipo} className='object-cover h-[200px] w-full'/>
                 <div className='flex flex-col justify-between mt-[15px] border border-gray-400 
                     w-full text-gray-600 CocoGothic_trial p-3'
+                    ref={index === 0 ? firstDivRef : index === 2 ? thirdDivRef : null} // Set refs for the first and third divs
                 >
                     <p>{item.capac}</p>
                     <div className='py-[20px'>
